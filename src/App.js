4db2b94c1032b413a272f453e5d00833ce1fa057
MSmallhouse <smallhou@bc.edu>
2024-01-11 17:35:23 -0500

    updating

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ import "./styles/styles.css";
 // Import and apply App specific css
 import css from "./styles/App.module.css"
 
-// The component that adds our Meta tags to the page
-import Seo from './components/seo.jsx';
-
 // App function that is reflected across the site
 export default function App() {
   const [page, setPage] = useState("main");
@@ -37,7 +34,6 @@ export default function App() {
     
   return (
     <>
-      <Seo />
       <div className={css.container}>
         <Navbar onButtonClick={handleClick} />
         <main role="main" className="wrapper">
